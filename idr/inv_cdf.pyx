3c8e1a0dac9cf62a25a3eb15f3751012232267d7
Yuya Nagaoka <92162607+YuNagaoka@users.noreply.github.com>
2025-07-09 16:11:31 +0900

    Update inv_cdf.pyx

--- idr/inv_cdf.pyx
+++ idr/inv_cdf.pyx
@@ -1,5 +1,6 @@
-cimport cython
+import numpy as np
 cimport numpy as np
+cimport cython
 from libc.math cimport exp, sqrt, pow, log, erf, fabs
 from libc.stdlib cimport malloc, free
 
@@ -8,19 +9,18 @@ cdef inline double d_min(double a, double b): return a if a <= b else b
 
 @cython.cdivision(True)
 cdef double c_cdf(double x, double mu, double sigma, double lamda):
-    norm_x = (x-mu)/sigma
-    return 0.5*lamda*erf(norm_x/sqrt(2)) + 0.5*(1-lamda)*erf(x/sqrt(2)) + 0.5
+    cdef double norm_x = (x - mu) / sigma
+    return 0.5 * lamda * erf(norm_x / sqrt(2)) + 0.5 * (1 - lamda) * erf(x / sqrt(2)) + 0.5
 
 @cython.cdivision(True)
 cdef double c_cdf_d1(double x, double mu, double sigma, double lamda):
     cdef double pi = 3.14159265358979323846264338327950288419716939937510582
-    cdef double pre = 1./sqrt(2*pi)
+    cdef double pre = 1. / sqrt(2 * pi)
 
-    cdef double noise = (1-lamda)*exp(-0.5*(x**2))
-
-    cdef double norm_x = (x - mu)/sigma
-    cdef double signal = lamda*exp(-0.5*(norm_x**2))
-    return pre*(signal + noise)
+    cdef double noise = (1 - lamda) * exp(-0.5 * (x ** 2))
+    cdef double norm_x = (x - mu) / sigma
+    cdef double signal = lamda * exp(-0.5 * (norm_x ** 2))
+    return pre * (signal + noise)
 
 @cython.cdivision(True)
 def cdf_d1(double x, double mu, double sigma, double lamda):
@@ -33,8 +33,10 @@ def cdf(double x, double mu, double sigma, double lamda):
 @cython.cdivision(True)
 cdef double c_cdf_i(double r, double mu, double sigma, double lamda, 
                     double lb, double ub, double EPS):
+    cdef int i
+    cdef double mid, guess
     for i in range(1000):
-        mid = lb + (ub - lb)/2.;
+        mid = lb + (ub - lb) / 2.
         guess = c_cdf(mid, mu, sigma, lamda)
         if fabs(guess - r) < EPS:
             return mid
@@ -42,18 +44,17 @@ cdef double c_cdf_i(double r, double mu, double sigma, double lamda,
             lb = mid
         else:
             ub = mid
-    
     return mid
 
 @cython.cdivision(True)
 def cdf_i(double r, double mu, double sigma, double lamda, 
           double lb, double ub, double EPS):
-    lb = d_min(0, mu) - 10/d_min(1, sigma)
-    ub = d_max(0, mu) + 10/d_min(1, sigma)
+    lb = d_min(0, mu) - 10 / d_min(1, sigma)
+    ub = d_max(0, mu) + 10 / d_min(1, sigma)
 
     while c_cdf(lb, mu, sigma, lamda) > r:
         lb -= 10
-    
+
     while c_cdf(ub, mu, sigma, lamda) < r:
         ub += 10
 
@@ -67,41 +68,40 @@ def cdf_i(double r, double mu, double sigma, double lamda,
 @cython.wraparound(False)
 @cython.cdivision(True)
 def c_compute_pseudo_values(
-        np.ndarray[np.int_t, ndim=1] rs, 
-        np.ndarray[np.double_t, ndim=1] zs, 
+        np.ndarray[np.intp_t, ndim=1] rs, 
+        np.ndarray[np.float64_t, ndim=1] zs, 
         double mu, double sigma, double lamda,
         double EPS):
     cdef int N = len(rs)
-    cdef double pseudo_N = N+1
-    cdef double* ordered_zs = <double * >malloc(N*sizeof(double))
-    
-    cdef double smallest_r = 1./pseudo_N
-    lb = d_min(0, mu)
+    cdef double pseudo_N = N + 1
+    cdef double* ordered_zs = <double *>malloc(N * sizeof(double))
+
+    cdef double smallest_r = 1. / pseudo_N
+    cdef double lb = d_min(0, mu)
     while c_cdf(lb, mu, sigma, lamda) > smallest_r:
         lb -= 1
 
-    ub = d_max(0, mu)
-    while c_cdf(ub, mu, sigma, lamda) < 1-smallest_r:
+    cdef double ub = d_max(0, mu)
+    while c_cdf(ub, mu, sigma, lamda) < 1 - smallest_r:
         ub += 1
-    
+
     lb = c_cdf_i(smallest_r, mu, sigma, lamda, lb, ub, EPS)
     ordered_zs[0] = lb
-    
-    cdef size_t i = 0, j= 0
+
+    cdef size_t i = 0
     cdef double r = 0
-    cdef double res = 10, prev_res = 10
+    cdef double res = 10
     for i in range(1, N):
-        r = (i+1)/pseudo_N
+        r = (i + 1) / pseudo_N
         if c_cdf(ub, mu, sigma, lamda) < r:
             ub += 10
         res = c_cdf_i(r, mu, sigma, lamda, lb, ub, EPS)
-        
         ordered_zs[i] = res
         lb = res
-        ub = lb + 2*(res - ordered_zs[i-1])
-    
+        ub = lb + 2 * (res - ordered_zs[i - 1])
+
     for i in range(N):
         zs[i] = ordered_zs[rs[i]]
-    free( ordered_zs )
-    
+
+    free(ordered_zs)
     return zs
